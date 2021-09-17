aeab2a863f2cd0203ad5576db82964fdfa6841c5
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-16 22:51:08 -0700

    added useCharacter hook into main

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import List from '../components/list/List';
+import useCharacters from '../services/Characters';
 
 const Main = () => {
+  const { loading, characters } = useCharacters();
 
+  return loading ? <>Loading...</> : <List characters={characters} />;
 };
 
 export default Main;
