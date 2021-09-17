a6700232a07b9b4adbbe53d0f7fcea97cc316440
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-16 22:29:26 -0700

    added struct of router in App

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Main from '../../containers/Main';
 
-export default function App() {
-  return <h1>Hello World</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/details/:name" component="" />
+        </Switch>
+      </Router>
+    </>
+  );
+};
+
+export default App;
