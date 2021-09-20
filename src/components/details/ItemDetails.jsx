25e64040acad441ddb18e72214413e3e9fa6a0c9
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-19 19:10:14 -0700

    updated ItemDetails and Css

--- src/components/details/ItemDetails.jsx
+++ src/components/details/ItemDetails.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import styles from './styles/ItemDetails.css';
 
 const ItemDetails = ({ character }) => {
   [character] = character;
   return (
-    <div>
-      {(character.pokemon)}
+    <main className={styles.ItemDetails}>
+      <heading>{(character.pokemon)}</heading>
       <img src={character.url_image} alt={(character.pokemon)} />
-      <div>
+      <section className={styles.details}>
         <p>Height: {character.height}</p>
         <p>Weight: {character.weight}</p>
         <p>Primary Type: {character.type_1}</p>
         <p>Secondary Type: {character.type_2}</p>
         <p>Pokebase: {character.pokebase}</p>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
 ItemDetails.propTypes = {
   character: PropTypes.array,
 };
-
 export default ItemDetails;
